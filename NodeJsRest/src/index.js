0a56b83610438bbb8d53e3de8ec6be192320f9f4
Erick <ericksac@zohomail.com>
2021-07-06 18:16:58 -0600

    eliminido

--- NodeJsRest/src/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./server/config');
-
-const http = require('http').Server(app);
-
-// Starting the server
-var server = http.listen(app.get('port'), () => {
-    console.log('Server on port 3000');
-  });
