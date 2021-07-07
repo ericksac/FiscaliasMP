9e9d069f287d75e36da02a94fb6bc84c7923dea4
Erick <ericksac@zohomail.com>
2021-07-06 19:00:02 -0600

    cambios borrados

--- tailwind-react/src/App.js
+++ tailwind-react/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header'
+import Fiscalias from './components/Fiscalias'
+import NuevaFiscalia from './components/NuevaFiscalia';
+import EditarFiscalia from './components/EditarFiscalia';
+
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Provider store = {store}>
+      <Header/>
+      <div className= "content-center pl-36 pr-36">
+        <Switch>
+          <Route exact path="/" component= {Fiscalias}/>
+          <Route exact path="/fiscalias/nuevo" component= {NuevaFiscalia}/>
+          <Route exact path="/fiscalias/editar/:id" component= {EditarFiscalia}/>
+        </Switch>
+      </div>
+      </Provider>
+    </Router>
+  )
 }
 
 export default App;
